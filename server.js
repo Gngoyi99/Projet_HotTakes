9f5621428279483252bba800fa75996f11e4e6c2
Gngoyi99 <73467477+Gngoyi99@users.noreply.github.com>
2022-04-19 18:20:58 +0200

    Delete server.js

--- server.js
+++
@@ -1,7 +0,0 @@
-const http = require("http");
-const app = require("./app");
-
-app.set("port", process.env.PORT || 3000);
-const server = http.createServer(app);
-
-server.listen(process.env.PORT || 3000);
