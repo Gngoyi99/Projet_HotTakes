8f46b2fb7107c39bfd3c18aa205a1d44e403f6ae
Gngoyi99 <73467477+Gngoyi99@users.noreply.github.com>
2022-04-19 18:13:37 +0200

    Add files via upload

---
+++ routes/sauce.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const router = express.Router();
+
+const sauceCtrl = require("../controllers/sauce");
+const auth = require("../middleware/auth");
+const multer = require("../middleware/multer-config");
+
+router.post("/", auth, multer, sauceCtrl.createSauce);
+
+router.put("/:id", auth, multer, sauceCtrl.modifySauce);
+
+router.delete("/:id", auth, sauceCtrl.deleteSauce);
+
+router.get("/:id", auth, sauceCtrl.getOneSauce);
+
+router.get("/", auth, sauceCtrl.getAllSauces);
+
+router.post("/:id/like", auth, sauceCtrl.likeDislike);
+
+module.exports = router;
